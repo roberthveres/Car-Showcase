acb0be37ba465c8c561970c58c389b0a8bfca7e0
veresrobert <veresrobert20@gmail.com>
2024-06-13 15:42:18 +0300

    creating Navbar and Footer components

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,10 +29,9 @@ const handleScroll = () => {
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-            <Image src="/hero.png" alt="hero" 
-            fill className="object-contain"/>
-            <div className="hero__image-overlay"></div>
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
         </div>
+        <div className="hero__image-overlay"></div>
       </div>
     </div>
   );
